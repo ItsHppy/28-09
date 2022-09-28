830dfe19c3b968692606596ddc5bb82c7be3fca9
N1ger69 <114093327+N1ger69@users.noreply.github.com>
2022-09-28 18:35:55 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+using namespace std;
+int main() {
+	/*
+	//if9
+	int a, b;
+	cin >> a >> b;
+	if (a > b) {
+		int t = a;
+		a = b;
+		b = t;
+	}
+	cout << a << " " << b << endl;
+	*/
+	/*
+	//if11
+	int a, b;
+	cin >> a >> b;
+	if (a > b) {
+		a = a;
+		b = a;
+		}
+	else if (a < b) {
+		a = b;
+		b = b;
+	}
+	else if (a = b) {
+		a = 0;
+		b = 0;
+	}
+	cout << a << endl;
+	cout << b << endl;
+	*/
+	/*
+	//if12
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a < b && a < c) {
+		cout << a << endl;
+	}
+	else if (b < a && b < c) {
+		cout << b << endl;
+	}
+	else if (c < a && c < b) {
+		cout << b << endl;
+	}
+	*/
+	/*
+	//if13
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a < b && a > c) {
+		cout << a << endl;
+	}
+	else if (b > a && b < c;) {
+		cout << b << endl;
+	}
+	else if (c > a && c < b) {
+		cout << c << endl;
+	}
+	*/
+	/*
+	//if14
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a < c && a < b && c > a && c > b) {
+		cout << a << " " << c << endl;
+	}
+	else if (b < c && b < a && a > c && a > b) {
+		cout << b << " " << a << endl;
+	}
+	else if (c < a && c < b && b > a && b > c) {
+		cout << c << " " << b << endl;
+	}
+	*/
+	/*
+	//if15
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a > b && a > c && b > c) {
+		cout << a + b << endl;
+	}
+	else if (b > c && b > a && c > a) {
+		cout << b + c << endl;
+	}
+	else if (c > a && c > b && a > b) {
+		cout << c + a << endl;
+	}
+	*/
+	/*
+	//if16
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (b = a++ && c = b++) {
+		a = a * 2;
+		b = b * 2;
+		c = c * 2;
+		cout << a << " " << b << " " << c << endl;
+	}
+	else {
+		a = -a;
+		b = -b;
+		c = -c;
+		cout << a << " " << b << " " << c << endl;
+	}
+	*/
+}
